2a3c4bed4220407395e62b559a1c30378ee0fc67
victornoel <vnoel890@gmail.com>
2023-07-03 11:46:37 -0400

    Added add student page with api call

--- src/redux/students/students.types.js
+++ src/redux/students/students.types.js
@@ -1,5 +1,7 @@
 const StudentActionTypes = {
-    FETCH_ALL_STUDENTS : "FETCH_ALL_STUDENTS"
+    FETCH_ALL_STUDENTS : "FETCH_ALL_STUDENTS",
+    CREATE_NEW_STUDENT : "CREATE_NEW_STUDENT",  
+
 }
 
 export default StudentActionTypes;
